a726bc2d78d8c811340f674985d6579a16ad8347
Lawrence Rivales <lrivales@gmail.com>
2021-09-16 10:40:16 -0400

    ASK-89556

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,24 +8,24 @@ $("#currentDay").append(date);
 
 var timeColor = function() {    
     var now = moment().format("ddd MMM D YYYY hh:00 A ZZ").toString();
-    // console.log(now);
+    console.log("Now: " + now);
 
     $(".time-label").each(function () {
         var timeLabel = $(this).text();
         var strTime = moment(timeLabel, ["h", "hh"])
         var strTime = moment(strTime).format("ddd MMM D YYYY hh:00 A ZZ").toString();
-        // console.log(strTime);
+        console.log("Time: " + strTime);
         
         // if now < time label
         if (now < strTime) {
-            // console.log("This is in the future");
+            console.log("This is in the future");
             // change color to green
             $(this).siblings().addClass("table-success");
         } 
         
         // if time label is now
         else if (now === strTime) {
-            // console.log("This is now!");
+            console.log("This is now!");
             // change color to red
             $(this).siblings().addClass("table-danger");
         }
@@ -33,7 +33,7 @@ var timeColor = function() {
         // otherwise
         else {
             // change color to gray
-            // console.log("This is in the past.");
+            console.log("This is in the past.");
             $(this).siblings().addClass("table-secondary");
         }
     });
@@ -45,6 +45,8 @@ $("td").on("click", function() {
     $(this).attr("contentEditable", "true");
 });
 
-$("td").on("blur", function() {
-    console.log($(".time-label").text());
+$(".btn").on("click", function(event) {
+    console.log($(event.target));
+    text = $(event.target).closest("td").text();
+    console.log(text);
 });
