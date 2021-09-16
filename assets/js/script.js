829475d69653f78b0c3064a7350d91c26265d705
Lawrence Rivales <lrivales@gmail.com>
2021-09-16 19:22:36 -0400

    add persistence functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,24 +8,20 @@ $("#currentDay").append(date);
 
 var timeColor = function() {    
     var now = moment().format("ddd MMM D YYYY HH").toString();
-    // console.log("Now: " + now);
 
     $(".time-label").each(function () {
         var timeLabel = $(this).attr("id");
         var strTime = moment(timeLabel, ["h", "hh"])
         var strTime = moment(strTime).format("ddd MMM D YYYY HH").toString();
-        // console.log("Time: " + strTime);
         
         // if now < time label
         if (now < strTime) {
-            // console.log("This is in the future");
             // change color to green
             $(this).siblings().addClass("table-success");
         } 
         
         // if time label is now
         else if (now === strTime) {
-            // console.log("This is now!");
             // change color to red
             $(this).siblings().addClass("table-danger");
         }
@@ -33,11 +29,30 @@ var timeColor = function() {
         // otherwise
         else {
             // change color to gray
-            // console.log("This is in the past.");
             $(this).siblings().addClass("table-secondary");
         }
     });
 };
+
+var loadEvents = function() {
+    // get events from local storage
+    events = JSON.parse(localStorage.getItem("events"));
+
+    if (!events) {
+        var events = {};
+    }
+
+    else {
+        // for each event
+        for (var i = 0; i < events.length; i++) {
+            var $storedId = events[i].time;
+            // write saved data into the correct row
+            $(".time-label[id|='"+$storedId+"'").next().text(events[i].data);
+        };
+    }
+};
+
+loadEvents();
 
 timeColor();
 
@@ -48,29 +63,19 @@ $("td").on("click", function() {
 $(".btn").on("click", function(event) {
     // Grab the element that was clicked
     var $this = $(event.target);
-    // console.log("$this: ", $this);
 
     // Grab the parent of the element that was clicked
     var $parent = $this.parent();
-    // console.log("$parent: ", $parent);
 
     // Grab the siblings of the parent of the element that was clicked
     var $tdEditable = $parent.siblings("td.text-data");
-    // console.log("$tdEditable: ", $tdEditable);
 
     var $tdTime = $parent.siblings("th.time-label");
-    // console.log("$tdTime: ", $tdTime);
-
-    // Grab the text from the siblings
-    // console.log("$tdEditable.text(): ", $tdEditable.text());
-    // console.log("$tdTime.text(): ", $tdTime.text());
 
     events.push({
-        time: $tdTime.text(),
+        time: $tdTime.attr("id"),
         data: $tdEditable.text()
     });
-
-    // console.log("events: ", events);
 
     localStorage.setItem("events", JSON.stringify(events));
 });
